507c93ffe0cc088392349977528b24f7e0bff551
278451 <278451@local>
2020-05-23 17:00:27 +0800

    loda file

---
+++ ziyang/load_data.py
@@ -0,0 +1,13 @@
+import os,sys
+sys.path.append('/home/aistudio/external-libraries')
+import nibabel as nib
+import numpy as np
+
+data_path = "/home/aistudio/work/data/test_data/"
+n = 0
+for file_name in os.listdir(data_path):
+    data = nib.load(data_path+file_name)
+    data = data.get_fdata()
+    print(data.shape)
+    n+=1
+print(n)
