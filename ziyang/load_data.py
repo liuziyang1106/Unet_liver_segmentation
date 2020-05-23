71c70a18f1fd16a4697e196d4fbd29e87b23dabc
278451 <278451@local>
2020-05-23 17:23:21 +0800

    add new branch and modife the load data.py

--- ziyang/load_data.py
+++ ziyang/load_data.py
@@ -4,10 +4,15 @@ import nibabel as nib
 import numpy as np
 
 data_path = "/home/aistudio/work/data/test_data/"
+gt_path = "/home/aistudio/work/data/gt/"
 n = 0
-for file_name in os.listdir(data_path):
-    data = nib.load(data_path+file_name)
-    data = data.get_fdata()
-    print(data.shape)
-    n+=1
+for gt_file_name in os.listdir(gt_path):
+    for train_file_name in os.listdir(data_path):
+        train_id_number = train_file_name.split("-")[1]
+        gt_id_number = gt_file_name.split("-")[1]
+        if train_id_number == gt_id_number:
+            data = nib.load(data_path+train_file_name)
+            data = data.get_fdata()
+            print("shape is equal: ",data.shape)
+            n+=1
 print(n)
